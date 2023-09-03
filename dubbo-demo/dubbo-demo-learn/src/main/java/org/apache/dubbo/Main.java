00c1cd50bbfde3494047c9649146e4dd7711003d
yichengdong <1685368363@qq.com>
2023-09-03 10:39:40 +0800

    feat(学习模块): 新增学习源码的模块

---
+++ dubbo-demo/dubbo-demo-learn/src/main/java/org/apache/dubbo/Main.java
@@ -0,0 +1,13 @@
+package org.apache.dubbo;
+
+/**
+ * @Description: Main
+ * @Author: yichengdong
+ * @CreateDate: 2023/9/3 10:36
+ * @Copyright : 豆浆油条个人非正式工作室
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
